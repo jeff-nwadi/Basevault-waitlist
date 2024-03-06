47fe9a19ffeb3eef8a9f86c979bcae364d0d97b1
jefferson nwadi <111717472+jeff-nwadi@users.noreply.github.com>
2024-03-06 14:12:57 +0100

    my first commit

---
+++ Basevalt-waitlist/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "../app.css";
+</script>
+
+<body class="flex justify-center place-items-center w-screen h-screen">
+    <slot />
+</body>
---
+++ Basevalt-waitlist/src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<main>
+    <div class="text-center">
+        
+        <div class="logo">
+            <h3>
+                Logo Ui
+            </h3>
+        </div>
+
+        <div class="main-content mt-[16px]">
+            <h2 class="text-[52px] font-bold heading">Join the waitlist</h2>
+
+            <form action="" class="mt-[44px]">
+                <div class="relative flex place-items-center text-[#A5A5A5] focus-within:text-black ml-[25px] input-field">
+                    <i class="fa fa-user absolute ml-5 text-[20px]"></i>
+                    <input type="text" class=" border-[#A7A7A7] border-none ring-1 ring-[#A7A7A7] focus:ring-black focus:ring-2 w-[346px] h-[63px] rounded-[13px] px-3 py-2 outline-none pr-5 pl-16" placeholder="Full Name...." required>
+                </div>
+
+                <div class="relative flex place-items-center text-[#A5A5A5] focus-within:text-black mt-[20px]  ml-[25px] input-field">
+                    <!-- <img src="mail.svg" alt="" class="w-[28px] h-[28px] absolute ml-3"> -->
+                    <i class="fa-regular fa-envelope absolute ml-5 text-[20px]"></i>
+                    <input type="email" class="border-[#A7A7A7] border-none ring-1 ring-[#A7A7A7] focus:ring-black focus:ring-2 w-[346px] h-[63px] rounded-[13px] px-3 py-2 outline-none pr-5 pl-16" placeholder="Email Address...." required>
+                </div>
+
+                <div class="button mt-[49px]">
+                    <button type="submit" class="relative bg-[#3777D8] w-[346px] h-[63px] rounded-[13px] text-[#fff] font-medium text-[19px]">
+                        <span class="">Join waitlist 
+                            <i class="fa fa-arrow-right ml-8 absolute mt-[5px]  cursor-pointer "></i>
+                        </span> 
+                    </button>
+                </div>
+            </form>
+        </div>
+       
+    </div>
+</main>
